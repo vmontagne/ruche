d6679ce70ad834bd642d9e1c2975d8855cd73b88
valentin <valentin@debian.Debian>
2020-09-27 15:53:34 +0200

    maj front + ajout graphe

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,31 @@
-import React, { useRef, useLayoutEffect, useState } from 'react'
+import React, { useRef, useLayoutEffect, useState, useEffect } from 'react'
 import Container from '@material-ui/core/Container'
 import Typography from '@material-ui/core/Typography'
 import styles from './index.module.css'
+import moment from 'moment'
 
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
-import { FitnessCenter, AcUnit, Speed, AccessAlarm } from '@material-ui/icons'
+import { FitnessCenter, AcUnit, Speed, AccessAlarm, Home, BrightnessLow, Opacity, Cloud, BatteryChargingFull } from '@material-ui/icons'
 import Chart from '../src/components/Chart'
 import StatValue from '../src/components/StatValue'
 
 export default function Index() {
   const [width, setWidth] = useState(null)
   const chartContainer = useRef(null)
+  const [measure, setMeasure] = useState({})
+  const [graph, setGraph] = useState([])
+
+  useEffect(() => {
+    fetch('http://192.168.1.27/api/index.php', { mode: 'cors' }).then((response) =>
+       response.json()
+    )
+    .then(data => {
+      console.log(data)
+      setMeasure(data.measure)
+      setGraph(data.graphs)
+    })
+  }, [])
   console.log(chartContainer)
   useLayoutEffect(() => {
     if (chartContainer.current) {
@@ -29,37 +43,71 @@ export default function Index() {
       </Grid>
       <div className={styles.spacer} />
       <Grid container spacing={3}>
-        <Grid item xs={6}>
+        <Grid item xs={4}>
           <Paper className={styles.paper}>
             <StatValue
               picto={<FitnessCenter fontSize="large" />}
               label="Poids"
-              value="65"
+              value={measure.poids}
             />
             <StatValue
               picto={<AcUnit fontSize="large" />}
-              label="Température"
-              value="56"
+              label="Température extérieure"
+              value={measure.temp_ext}
+            />
+            <StatValue
+              picto={<Home fontSize="large" />}
+              label="Température intérieure"
+              value={measure.temp_int}
             />
           </Paper>
         </Grid>
-        <Grid item xs={6}>
+        <Grid item xs={4}>
           <Paper className={styles.paper}>
             <StatValue
-              picto={<Speed fontSize="large" />}
-              label="Vent"
-              value="45"
+              picto={<BrightnessLow fontSize="large" />}
+              label="Luminosité"
+              value={measure.luminosite}
+            />
+            <StatValue
+              picto={<Opacity fontSize="large" />}
+              label="Humidité"
+              value={measure.humidite}
+            />
+            <StatValue
+              picto={<Cloud fontSize="large" />}
+              label="Pression athmosphérique"
+              value={measure.pression_ath}
+            />
+          </Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={styles.paper}>
+            <StatValue
+              picto={<BatteryChargingFull fontSize="large" />}
+              label="Tension"
+              value={measure.tension}
+            />
+            <StatValue
+              picto={<BatteryChargingFull fontSize="large" />}
+              label="Intensité"
+              value={measure.intensite}
             />
             <StatValue
               picto={<AccessAlarm fontSize="large" />}
               label="Heure"
-              value="12:45"
+              value={moment(measure.date).format('DD/MM/Y HH:mm')}
             />
           </Paper>
         </Grid>
         <Grid item xs={12}>
           <Paper className={styles.paper} ref={chartContainer}>
-            <Chart width={width} />
+            <Chart width={width} data={graph.month} unitFormat="DD/MM/Y" />
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={styles.paper} ref={chartContainer}>
+            <Chart width={width} data={graph.day}  unitFormat="HH:mm"/>
           </Paper>
         </Grid>
       </Grid>
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,61 +1,62 @@
 import React from 'react'
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts'
-const data = [
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip,Legend } from 'recharts'
+import styles from './StatValue.module.css'
+import moment from 'moment'
+const mockData = [
   {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
+    t: '2020-01-01',
+    weight: 20,
+    temp: 19,
   },
   {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
+    t: '2020-02-01',
+    weight: 35,
+    temp: 22,
   },
   {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
+    t: '2020-03-01',
+    weight: 40,
+    temp: 26,
   },
   {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
+    t: '2020-04-01',
+    weight: 45,
+    temp: 15,
   },
   {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
+    t: '2020-05-01',
+    weight: 50,
+    temp: 25,
   },
   {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
+    t: '2020-06-01',
+    weight: 60,
+    temp: 29,
   },
   {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
+    t: '2020-07-01',
+    weight: 70,
+    temp: 35,
   },
 ]
 
-const Chart = ({ width }) => {
-  console.log(width, 'maxWidth')
+const Chart = ({ width, data, unitFormat }) => {
+  const formatUnits = (value) => {
+    return moment(value).format(unitFormat)
+  }
+  const tooltipFormatter = (value) => {
+    return moment(value).format(unitFormat)
+  }
   return (
     <LineChart width={width} height={width / 3} data={data}>
-      <Line type="monotone" dataKey="uv" stroke="#8884d8" yAxisId="uv" />
-      <YAxis orientation="right" dataKey="uv" yAxisId="uv" />
-      <Line type="monotone" dataKey="pv" stroke="#880000" />
-      <YAxis orientation="left" dataKey="pv" />
+      <Line type="monotone" dataKey="poids" stroke="#8884d8" yAxisId="poids" name="poids" />
+      <YAxis orientation="right" dataKey="poids" yAxisId="poids"/>
+      <Line type="monotone" dataKey="temp_ext" stroke="#880000" name="temperature" />
+      <YAxis orientation="left" dataKey="temp_ext" />
       <CartesianGrid stroke="#ccc" />
-      <XAxis dataKey="name" />
-      <Tooltip />
+      <Legend verticalAlign="top" height={36} />
+      <XAxis dataKey="date" tickFormatter={formatUnits} />
+      <Tooltip labelFormatter={tooltipFormatter} />
     </LineChart>
   )
 }
